9f03fac5068415bd96085923508778cb422be311
ankr19 <ankm335@gmail.com>
2022-08-18 21:49:40 +0530

    nograph api

---
+++ app.js
@@ -0,0 +1,30 @@
+/*
+
+it will install all the important files
+-> npm install 
+for starting the server we are going to use the 
+-> npm run nodemon 
+i have given the url detail for that you can used on postman
+this is json detail for the geting the stock from database;
+
+*/ 
+const express = require("express");
+const connect = require("./databases/db");
+const cors = require('cors');
+connect();
+
+const app = express();
+const port = 5000;
+
+app.use(cors());
+app.use(express.json());
+
+// giving the route for API ;
+app.use("/api/stockexchange", require("./routes/stockexchange"));
+
+app.listen(port, () => {
+  console.log(`app running on port http://localhost:${port}`);
+});
+
+
+
---
+++ databases/db.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+//const mongoDburl = "mongodb://localhost:27017/stockcollection?readPreference=primary&appname=MongoDB%20Compass&ssl=false";
+const mongoDburl = "mongodb+srv://Anand:Kumar@cluster0.28kkaf4.mongodb.net/?retryWrites=true&w=majority";
+
+const connectToMongo = async () => {
+    mongoose.connect(mongoDburl, ()=> {
+        console.log("connected to mongo Successfully");
+    })
+}
+
+module.exports = connectToMongo;
